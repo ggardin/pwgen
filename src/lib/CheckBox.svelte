1c8af20a744e0b6cd7d26215cc6e6f1210cf5a03
Giovanni <2914344+ggardin@users.noreply.github.com>
2022-10-25 15:41:45 +0200

    Change forked components to suit requirements

--- src/lib/CheckBox.svelte
+++ src/lib/CheckBox.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-    // Props
-    export let checked: boolean = false;
-    export let id: string = "";
-    export let text: string = ""
-    // CreateDispatcher
-    import { createEventDispatcher } from "svelte";
-    const dispatch = createEventDispatcher();
-    function dispatchCheck(index: string){
-        dispatch("checked", index) 
-        checked = !checked
-    }
+
+	export let checked: boolean = false;
+	export let id: string = "";
+	export let text: string = "";
+
+	// CreateDispatcher
+	import { createEventDispatcher } from "svelte";
+	const dispatch = createEventDispatcher();
+	function dispatchCheck(index: string){
+		dispatch("checked", index);
+		checked = !checked;
+	}
+
 </script>
 
 <div>
@@ -31,12 +33,3 @@
 	}
 
 </style>
-
-<div class="filterBox">
-    <input type="checkbox" {id}>
-    <span class="checkbox" {id} class:checked={checked} on:click={dispatchCheck}>
-        <svg width="14" height="12" xmlns="http://www.w3.org/2000/svg"><path stroke="#18171F" stroke-width="3" fill="none" d="M1 5.607 4.393 9l8-8"/></svg>
-    </span>
-    
-    <label for="{id}">{text}</label>
-</div>
