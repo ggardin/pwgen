8209fbefa62e4ac2a8056a6c1b16f5f9efc422a4
Giovanni <2914344+ggardin@users.noreply.github.com>
2022-10-25 15:40:50 +0200

    Introduce Header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,17 @@
+<header>
+	<h1>Password Generator Tool</h1>
+</header>
+
+<style>
+
+	header {
+		background-color: var(--shade);
+		padding: var(--padding);
+	}
+
+	h1 {
+		font-size: 1em;
+		line-height: 1.2em;
+	}
+
+</style>
