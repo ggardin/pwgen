bb7f09b4df9ffdb88bf03c1b3f63fd9ff581b77c
Giovanni <2914344+ggardin@users.noreply.github.com>
2022-10-26 09:54:20 +0200

    Add ARIA labels

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,4 +1,4 @@
-<button on:click>
+<button on:click aria-label="Generate password and copy">
 	Generate & Copy
 </button>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -104,12 +104,13 @@
 	<section class="masthead {passwordStrength}">
 		<Header />
 		<div class="password">
-			<input type="text" value={passwordString} readonly/>
+			<input type="text" value={passwordString} readonly aria-label="The generated password"/>
 			<div class="action">
 				<button
 				class:disabled={copied || passwordString.length === 0}
 				on:click={passwordGenerator.copyToClipboard}
 				on:keypress={passwordGenerator.copyToClipboard}
+				aria-label="Copy password"
 				>
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect width="13" height="13" x="9" y="9" rx="2" ry="2"/><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/></svg>
 				</button>
@@ -149,7 +150,7 @@
 					getRandomNumber,
 					getRandomSymbol
 				)}
-			on:click={passwordGenerator.copyToClipboard}
+			aria-label="Password length"
 		/>
 	</section>
 	<p class="toast" class:active={copied}>
